d8060425a9758600ddb1cef37e7427f7ca491198
Soeun Yi <siraasony@gmail.com>
2020-02-04 17:51:01 +0900

    done for 04.02.20

--- samsung/samsung05_ensemble_LSTM.py
+++ samsung/samsung05_ensemble_LSTM.py
@@ -4,110 +4,111 @@ import pandas as pd
 samsung = np.load('./data/samsung.npy')
 kospi200 = np.load('./data/kospi200.npy')
 
-print (kospi200)
-print (kospi200.shape)
-
+print(samsung) #(426, 5)
+print(kospi200) #(426, 5)
+print(samsung.shape) #(426, 5)
+print(kospi200.shape) #(426, 5)
 
 def split_xy5(dataset, time_steps, y_column):
     x, y = list(), list()
-    for i in range(len(dataset)) :
-        x_end_number = i + time_steps
-        y_end_number = x_end_number + y_column
+    for i in range(len(dataset)):
+        x_end_number = i + time_steps   
+        y_end_number = x_end_number + y_column   
         
         if y_end_number > len(dataset) :
             break
-        tmp_x = dataset[i:x_end_number, :]
-        tmp_y = dataset[x_end_number:y_end_number, 3]
-        x.append(tmp_x)
+        tmp_x = dataset[i:x_end_number, :] # x값(5x5)
+        tmp_y = dataset[x_end_number : y_end_number, 3] # y값(종가)
+        x.append(tmp_x) 
         y.append(tmp_y)
     return np.array(x), np.array(y)
 
-
 x1, y1 = split_xy5(samsung, 5, 1)
 x2, y2 = split_xy5(kospi200, 5, 1)
-# print (x.shape)
-# print (y.shape)
-# print (x[0,:], '\n', y[0])
+print(x1.shape) #(421, 5, 5)
+print(y1.shape) #(421, 1)
+print(x1[0, :], '\n', y1[0])
 
-# ================================묶음으로 잘라내기================================#
-
+# 데이터셋 나누기
 from sklearn.model_selection import train_test_split
 x1_train, x1_test, y1_train, y1_test = train_test_split(x1, y1, random_state = 1, test_size = 0.3, shuffle = False)
 x2_train, x2_test, y2_train, y2_test = train_test_split(x2, y2, random_state = 1, test_size = 0.3, shuffle = False)
 
-# print (x1_train.shape)
-# print (x1_test.shape)
+print(x1_train.shape) #(294, 5, 5)
+print(x1_test.shape)  #(127, 5, 5)
+print(x2_train.shape) #(294, 5, 5)
+print(x2_test.shape)  #(127, 5, 5)
+print(y1_train.shape) #(294, 5, 5)
+print(y1_test.shape)  #(127, 5, 5)
 
 # 데이터 전처리
-# standardscaler
-
-x1_train = np.reshape(x1_train, (x1_train.shape[0], x1_train.shape[1] * x1_train.shape[2]))
-x1_test = np.reshape(x1_test, (x1_test.shape[0], x1_test.shape[1] * x1_test.shape[2]))
-
-x2_train = np.reshape(x2_train, (x2_train.shape[0], x2_train.shape[1] * x2_train.shape[2]))
-x2_test = np.reshape(x2_test, (x2_test.shape[0], x2_test.shape[1] * x2_test.shape[2]))
-
-# print (x_train.shape)
-# print (x_test.shape)
+# StandardScaler
+x1_train = x1_train.reshape(x1_train.shape[0], x1_train.shape[1]*x1_train.shape[2])
+x1_test = x1_test.reshape(x1_test.shape[0], x1_test.shape[1]*x1_test.shape[2])
+x2_train = x2_train.reshape(x2_train.shape[0], x2_train.shape[1]*x2_train.shape[2])
+x2_test = x2_test.reshape(x2_test.shape[0], x2_test.shape[1]*x2_test.shape[2])
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaler.fit(x1_train)
-x1_train_scaled = scaler.transform(x1_train)
-x1_test_scaled = scaler.transform(x1_test)
-# print (x_train_scaled[0, :])
-# print (x_train_scaled.shape)
-scaler = StandardScaler()
+x1_train = scaler.transform(x1_train)
+x1_test = scaler.transform(x1_test)
+print(x1_train[0, :])
+
 scaler.fit(x2_train)
-x2_train_scaled = scaler.transform(x2_train)
-x2_test_scaled = scaler.transform(x2_test)
+x2_train = scaler.transform(x2_train)
+x2_test = scaler.transform(x2_test)
+print(x2_train[0, :])
 
+x1_train =  x1_train.reshape(294, 5, 5)
+x1_test = x1_test.reshape(127, 5, 5)
+x2_train =  x2_train.reshape(294, 5, 5)
+x2_test = x2_test.reshape(127, 5, 5)
 
-# ================================전처리 끝================================#
-
-x1_train_scaled = x1_train_scaled.reshape(x1_train_scaled.shape[0],5, 5)
-x1_test_scaled = x1_test_scaled.reshape(x1_test_scaled.shape[0], 5, 5)
-
-x2_train_scaled = x2_train_scaled.reshape(x2_train_scaled.shape[0],5, 5)
-x2_test_scaled = x2_test_scaled.reshape(x2_test_scaled.shape[0], 5, 5)
-
-
-#print (x_train_scaled.shape)
-
-#LSTM 모델 (Dense) 구성
+# 모델
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input, LSTM
+from keras.layers import Dense, LSTM, Input
 
-input1 = Input(shape=(5,5))
+# model 1
+input1 = Input(shape=(5, 5))
 dense1 = LSTM(64)(input1)
-dense2 = Dense(32)(dense1)
-dense3 = Dense(32)(dense2)
-output1 = Dense(1)(dense3)
+dense1 = Dense(32)(dense1)
+dense1 = Dense(32)(dense1)
+dense1 = Dense(32)(dense1)
+output1 = Dense(1)(dense1)
 
-input2 = Input(shape=(5,5))
-dense21 = Dense(64)(input2)
-dense23 = Dense(32)(dense21)
-output2 = Dense(1)(dense23)
+# model 2
+input2 = Input(shape=(5, 5))
+dense2 = LSTM(64)(input2)
+dense2 = Dense(32)(dense2)
+dense2 = Dense(32)(dense2)
+dense2 = Dense(32)(dense2)
+output2 = Dense(1)(dense2)
 
-from keras.layers.merge import concatenate
-from keras.layers import Concatenate
+from keras.layers import Concatenate, concatenate
+merge1 = concatenate([output1, output2])
+middle1 = Dense(16)(merge1)
+middle2 = Dense(8)(middle1)
+output = Dense(1)(middle2)
 
-#merge1 = concatenate([output1, output2]) #모델을 사슬처럼 엮다.
-merge1 = Concatenate()([output1, output2])
-output3 = Dense(1)(merge1)
-model = Model(inputs = [input1, input2], outputs = output3)
+model = Model(inputs = [input1, input2], outputs = output)
+model.summary()
 
-
+model.compile(loss='mse', optimizer='adam', metrics=['mse']) # mse, mae 사용
 from keras.callbacks import EarlyStopping
 early_stopping = EarlyStopping(patience=20)
-model.compile(loss='mse', optimizer='adam', metrics=['mse'])  
-model.fit([x1_train_scaled, x2_train_scaled], y1_train, validation_split=0.2, epochs=100, batch_size=1, verbose =1, callbacks=[early_stopping])  
+model.fit([x1_train, x2_train], y1_train, epochs=50, batch_size = 1, validation_split = 0, callbacks=[early_stopping]) 
 
-loss, mse = model.evaluate([x1_test_scaled,x2_test_scaled], y1_test, batch_size=1)
-print("loss:",loss,"mse:", mse)
+loss, mse = model.evaluate([x1_test, x2_test], y1_test, batch_size = 1)
+print('loss:', loss)
 
-y_pred = model.predict([x1_test_scaled,x2_test_scaled])
-
+y_pred = model.predict([x1_test, x2_test])
 
 for i in range(5):
-    print('종가:', y1_test[i], '/예측가:', y_pred[i])
+    print('종가:', y1_test[i], 'y예측값:', y_pred[i])
+    
+# RMSE
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_pred):
+    return np.sqrt(mean_squared_error(y1_test, y_pred))
+print('RMSE : ', RMSE(y1_test, y_pred))
